1abef0eb77d84a39db9d3c8eba50bf145e30dea0
Manoj Kumar Paladugu <paladugumanojkumar@gmail.com>
2024-11-03 00:42:36 +0530

    feat: Added zephyr project file

--- Application/Main.c
+++ Application/Main.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
+
 int main(void)
 {
-    return 0;
+    printf("hello world\n");
 }
---
+++ run-qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -vga none -net none -semihosting -kernel _out/qemu/zephyr/stm32g491re.elf
